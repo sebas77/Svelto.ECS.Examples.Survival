138b4a6aa11290fcd74b814f8440ca9311c561d0
sebas77 <ssense@tiscali.it>
2017-12-22 23:58:10 +0000

    reconstruct submodule

--- Assets/Plugins/Svelto-ECS/ECS/NodeSubmissionScheduler.cs
+++
@@ -1,9 +0,0 @@
-using Svelto.WeakEvents;
-
-namespace Svelto.ECS.Schedulers
-{
-    public abstract class EntityViewSubmissionScheduler
-    {
-        abstract public void Schedule(WeakAction submitEntityViews);
-    }
-}
